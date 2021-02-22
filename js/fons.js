2e9e088588889fd5c455f47223c6ebfff58c63a7
Frank Palomino <frank.palomino@gmail.com>
2021-02-22 17:21:42 +0100

    js refactorizado

---
+++ js/fons.js
@@ -0,0 +1,5 @@
+function codeAddress(){
+  document.body.style.backgroundImage = "url('img/1.jpg')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "cover";
+}
